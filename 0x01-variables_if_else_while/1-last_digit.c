a70e4c8dd970c35c5e8017d494a3be84f9c8d3a5
tobidavi <fatokunoluwatobi9@gmail.com>
2022-09-11 14:35:42 -0700

    A program that determines if the last digit of a random number is greater or less than 5

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Detrmines if the last digit of a random is greater or less than 6
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int a;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	a = n % 10;
+	if (a > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
+	}
+	else if (a == 0)
+	{
+		printf("Last digit of %d is %d is 0\n", n, a);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6\n", n, a); 
+	}
+	return (0);
+}
