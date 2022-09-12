d7df8eebb5043d0a5a2e54807e652fc9554c1872
tobidavi <fatokunoluwatobi9@gmail.com>
2022-09-12 09:42:12 -0700

    a program that prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Prints all combination of a single digit number
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int q;
+
+	for (q = 0; q <=10; q++)
+	{
+		putchar(q);
+		if (q != 10)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
