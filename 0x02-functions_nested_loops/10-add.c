f23beadcacd4a8767e8313e3403875c7eb3ab941
tobidavi <fatokunoluwatobi9@gmail.com>
2022-09-14 22:20:43 -0700

    a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two numbers from input
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	return(a + b);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,7 +28,7 @@ void times_table(void)
 				_putchar(' ');
 			}
 			_putchar((d % 10) + '0');
-			if (c < 9);
+			if (c < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
